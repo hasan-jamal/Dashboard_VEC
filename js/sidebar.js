7eb7e83cc9098f9a88c97c1b9e65179c74a3289c
Hasan Jamal <hasanjamalabusharekh@gmail.com>
2023-03-14 11:41:12 +0200

    Update-des

--- js/sidebar.js
+++ js/sidebar.js
@@ -16,3 +16,21 @@ function myFunction(x) {
    var x = window.matchMedia("(max-width: 700px)")
    myFunction(x)
    x.addListener(myFunction) 
+
+   // DropDown Sidebar 
+   var dropdown = document.getElementsByClassName("dropdown-btn");
+   var dropdownArrow = document.getElementById("arrowDrop");
+   console.log(dropdownArrow);
+   var i;
+   for (i = 0; i < dropdown.length; i++) {
+       dropdown[i].addEventListener("click", function() {
+       var dropdownContent = this.nextElementSibling;
+       if (dropdownContent.style.display === "flex") {
+         dropdownContent.style.display = "none";
+         dropdownArrow.style.transform = "rotate(180deg)";
+       } else {
+         dropdownContent.style.display = "flex";
+         dropdownArrow.style.transform = "rotate(0deg)";
+       }
+     });
+   } 
